5ac8adb01e0c4ab74e7a9343cfe29a85c171bf12
Muhammed-Rahif <rahifpalliyalil@gmail.com>
2024-04-19 01:29:51 +0530

    setup(project): capacitorjs sveltekit

---
+++ android/app/src/main/java/tech/rahif/quran/MainActivity.java
@@ -0,0 +1,5 @@
+package tech.rahif.quran;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'tech.rahif.quran',
+  appName: 'Quran',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
